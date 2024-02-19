ca7b8737abeb4f406331f9be84744f3c619bd5ca
acegoal07 <acegoal07@gmail.com>
2024-02-19 16:54:31 +0000

    Adds user features

--- scripts.js
+++ scripts.js
@@ -3,65 +3,74 @@ window.addEventListener("load", () => {
    document.querySelector("#search-form").addEventListener("submit", async (event) => {
       event.preventDefault();
 
+      document.querySelector("#search_results_output").querySelectorAll("*").forEach(n => n.remove());
+
       // Get values from the form
+      var page_size = document.querySelector("#size-select").value;
       var search_query = document.querySelector("#search_query").value;
    
       // Get handle to the hint messages
       const hint_search_query = document.querySelector("#hint_search_query");
+      const hint_page_size_selector = document.querySelector("#hint_page_size_selector");
    
       let fields_valid = true;
    
       if (search_query === "" || search_query.length < 0) {
-         hint_search_query.style.display = "inline";
+         hint_search_query.classList.add("show-inline");
          fields_valid = false;
       } else {
-         hint_search_query.style.display = "none";
+         hint_search_query.classList.remove("show-inline");
       }
 
-      document.querySelector("#search-form").style.display = "none";
-      document.querySelector("#loading-view").style.display = "block";
+      if (page_size === "" || page_size.length < 0) {
+         hint_page_size_selector.classList.add("show-inline");
+         fields_valid = false;
+      } else {
+         hint_page_size_selector.classList.remove("show-inline");
+      }
       
       if (fields_valid) {
-         await fetch('https://api.vam.ac.uk/v2/objects/search?q=' + search_query)
+         document.querySelector("#search-form").classList.remove("show-block");
+         document.querySelector("#loading-view").classList.add("show-block");
+         await fetch(`https://api.vam.ac.uk/v2/objects/search?q=${search_query}&page_size=${page_size}&data_profile=full`)
             .then(async response => {
                if (response.ok) {
                   const response_data = await response.json();
-
-                  console.log(response_data);
                   const records_data = response_data.records;
-                  console.log(records_data[0]);
                   // loop through the records and display the results
                   records_data.forEach(record => {
-                     const record_div = document.createElement("div");
-                     record_div.classList.add("record");
-                     record_div.innerHTML = `
-                        <h2>Record ${record.systemNumber}</h2>
-                        <p>Piece name: ${record._primaryTitle}</p>
-                        <p>Maker: ${record._primaryMaker.name}</p>
+                     const record_div = `
+                        <div class="record_display">
+                           <h2>Record ${record.systemNumber}</h2>
+                           ${record._images._primary_thumbnail === undefined ? "<p>No image available</p>" : `<img src='${record._images._primary_thumbnail}' alt='${record._primaryTitle}'/>`}
+                           <p>Title: ${record._primaryTitle === '' ? "No title" : record._primaryTitle}</p>
+                           <p>Date: ${record._primaryDate === '' ? "No date" : record._primaryDate}</p>
+                           <p>Description: ${record.summaryDescription === '' ? "no description available" : record.summaryDescription}</p>             
+                        </div>
                      `;
-                     document.querySelector("#search-results").appendChild(record_div);
+                     document.querySelector("#search_results_output").insertAdjacentHTML("afterbegin", record_div);
                   });
-
-                  document.querySelector("#loading-view").style.display = "none";
-                  document.querySelector("#search-results").style.display = "block";
+                  document.querySelector("#loading-view").classList.remove("show-block");
+                  document.querySelector("#search-results").classList.add("show-block");
                } else {
-                  document.querySelector("#loading-view").style.display = "none";
-                  document.querySelector("#error-view").style.display = "block";
+                  document.querySelector("#loading-view").classList.remove("show-block");
+                  document.querySelector("#error-view").classList.add("show-block");
                }
             }).catch(error => {
-               document.querySelector("#loading-view").style.display = "none";
-               document.querySelector("#error-view").style.display = "block";
+               document.querySelector("#loading-view").classList.remove("show-block");
+               document.querySelector("#error-view").classList.add("show-block");
             });
       }
    });
 
    document.querySelector("#try-again").addEventListener("click", () => {
-      document.querySelector("#error-view").style.display = "none";
-      document.querySelector("#search-form").style.display = "block";
+      document.querySelector("#error-view").classList.remove("show-block");
+      document.querySelector("#search-form").classList.add("show-block");
    });
 
    document.querySelector("#back-to-search").addEventListener("click", () => {
-      document.querySelector("#search-results").style.display = "none";
-      document.querySelector("#search-form").style.display = "block";
+      document.querySelector("#search_query").value = "";
+      document.querySelector("#search-results").classList.remove("show-block");
+      document.querySelector("#search-form").classList.add("show-block");
    });
 });
