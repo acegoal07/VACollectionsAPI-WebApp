0ea39f89ae87b0bab52e5061395e6c648a62c3c3
acegoal07 <acegoal07@gmail.com>
2024-02-21 13:39:58 +0000

    Security

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
 window.addEventListener("load", () => {
    document.querySelector("#search-form").addEventListener("submit", async (event) => {
       event.preventDefault();
-
+      
+      // Clear previous search results 
       document.querySelector("#search_results_output").querySelectorAll("*").forEach(n => n.remove());
 
       // Get values from the form
       var page_size = document.querySelector("#size-select").value;
+      var image_required = document.querySelector("#image_check").checked;
       var search_query = document.querySelector("#search_query").value;
-   
+
       // Get handle to the hint messages
-      const hint_search_query = document.querySelector("#hint_search_query");
       const hint_page_size_selector = document.querySelector("#hint_page_size_selector");
-   
+      const hint_search_query = document.querySelector("#hint_search_query");
+
       let fields_valid = true;
       // Check if the query input is valid
       if (search_query === "" || search_query.length < 0) {
@@ -27,45 +29,109 @@ window.addEventListener("load", () => {
       } else {
          hint_page_size_selector.classList.remove("show-inline");
       }
-      
+
       if (fields_valid) {
+         // Hide the search form and show the loading view
          document.querySelector("#search-form").classList.remove("show-block");
          document.querySelector("#loading-view").classList.add("show-block");
-         await fetch(`https://api.vam.ac.uk/v2/objects/search?q=${search_query}&page_size=${page_size}&data_profile=full`)
-            .then(async response => {
+         // Fetch the data from the API
+         await fetch(`https://api.vam.ac.uk/v2/objects/search?q=${search_query}&page_size=${page_size}&data_profile=full${image_required ? "&images_exist=true" : ""}`)
+         .then(async response => {
                if (response.ok) {
-                  const response_data = await response.json();
-                  const records_data = response_data.records;
-                  records_data.forEach(record => {
-                     document.querySelector("#search_results_output").insertAdjacentHTML("afterbegin", `
-                        <div class="record_display">
-                           <h2>Record ${record.systemNumber}</h2>
-                           ${record._images._primary_thumbnail === undefined ? "<strong><p>No image available</p></strong>" : `<img src='${record._images._primary_thumbnail}' alt='${record._primaryTitle}'/>`}
-                           <p><strong>Title:</strong> ${record._primaryTitle === '' ? "No title" : record._primaryTitle}</p>
-                           <p><strong>Date:</strong> ${record._primaryDate === '' ? "No date" : record._primaryDate}</p>
-                           <p><strong>Summary description:</strong> ${record.summaryDescription === '' ? "No description available" : record.summaryDescription}</p>             
-                        </div>
-                     `);
-                  });
-                  document.querySelector("#loading-view").classList.remove("show-block");
-                  document.querySelector("#search-results").classList.add("show-block");
+                  return await response.json();
                } else {
                   document.querySelector("#loading-view").classList.remove("show-block");
                   document.querySelector("#error-view").classList.add("show-block");
                }
+            }).then(data => {
+               const records_data = data.records;
+               const search_results_output = document.querySelector("#search_results_output");
+               if (records_data.length === 0) {
+                  let div = document.createElement("div");
+                  div.setAttribute("class", "record_display");
+
+                  let h2 = document.createElement("h2");
+                  h2.textContent = "No records found";
+                  div.appendChild(h2);
+
+                  let p = document.createElement("p");
+                  p.textContent = "There are no records that match your search query";
+                  div.appendChild(p);
+
+                  search_results_output.appendChild(div);
+               } else {
+                  records_data.forEach(async record => {
+                     let div = document.createElement("div");
+                     div.setAttribute("class", "record_display");
+
+                     let h2 = document.createElement("h2");
+                     h2.textContent = `Record ${record.systemNumber}`;
+                     div.appendChild(h2);
+
+                     await fetch(record._images._primary_thumbnail)
+                        .then(async response => {
+                           if (response.ok) {
+                              return await response.blob();
+                           } else {
+                              let img_error = document.createElement("p");
+                              img_error.textContent = "No image available";
+                              div.appendChild(img_error);
+                           }
+                        }).then(blob => {
+                           let img = document.createElement("img");
+                           img.src = URL.createObjectURL(blob);
+                           img.alt = record._primaryTitle;
+                           div.appendChild(img);
+                        }).catch(error => {
+                           let img_error = document.createElement("p");
+                           img_error.textContent = "No image available";
+                           div.appendChild(img_error);
+                        });
+
+                     let title_header = document.createElement("h3");
+                     title_header.textContent = "Title:";
+                     div.appendChild(title_header);
+                     let title = document.createElement("p");
+                     title.textContent = `${record._primaryTitle === '' ? "No title" : record._primaryTitle}`;
+                     div.appendChild(title);
+
+                     let date_header = document.createElement("h3");
+                     date_header.textContent = "Date:";
+                     div.appendChild(date_header);
+                     let date = document.createElement("p");
+                     date.textContent = `${record._primaryDate === '' ? "No date" : record._primaryDate}`;
+                     div.appendChild(date);
+
+                     let summary_description_header = document.createElement("h3");
+                     summary_description_header.textContent = "Summary description:";
+                     div.appendChild(summary_description_header);
+                     let summary_description = document.createElement("p");
+                     summary_description.textContent = `${record.summaryDescription === '' ? "No description available" : record.summaryDescription.replaceAll(/<[^>]*>?/gm, '')}`;
+                     div.appendChild(summary_description);
+
+                     search_results_output.appendChild(div);
+                  });
+               }
+               document.querySelector("#loading-view").classList.remove("show-block");
+               document.querySelector("#search-results").classList.add("show-block");
             }).catch(error => {
                document.querySelector("#loading-view").classList.remove("show-block");
                document.querySelector("#error-view").classList.add("show-block");
+               console.error(error);
             });
       }
    });
 
-   document.querySelector("#try-again").addEventListener("click", () => {
+   // Event listeners for the error view button to return to search screen
+   document.querySelector("#try-again").addEventListener("click", (event) => {
+      event.preventDefault();
       document.querySelector("#error-view").classList.remove("show-block");
       document.querySelector("#search-form").classList.add("show-block");
    });
 
-   document.querySelector("#back-to-search").addEventListener("click", () => {
+   // Event listener search result screen to start another search
+   document.querySelector("#back-to-search").addEventListener("click", (event) => {
+      event.preventDefault();
       document.querySelector("#search_query").value = "";
       document.querySelector("#search-results").classList.remove("show-block");
       document.querySelector("#search-form").classList.add("show-block");
